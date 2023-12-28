b4161dba6ddaaf32603b11e1f063c59f9238adad
Edge Quintanilla <equintanilla@discoveryed.com>
2023-12-28 11:37:16 -0600

    refactoring todo list to use composition API

--- app.vue
+++ app.vue
@@ -1,30 +1,18 @@
-<script>
-import { defineNuxtComponent } from '#app'
-
-export default defineNuxtComponent({
-  data: () => {
-    return {
-      todoList: []
-    }
-  },
-  methods: {
-    fetchTodoList() {
-      fetch('https://jsonplaceholder.typicode.com/todos')
-        .then(response => response.json())
-        .then(json => {
-          this.todoList = json
-        });
-    }
-  },
-  mounted() {
-    this.fetchTodoList();
-  }
-})
+<script setup>
+let todoList = ref([]);
+function fetchTodoList() {
+  fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(response => response.json())
+    .then(json => {
+      todoList.value = json
+    });
+}
 </script>
 
 <template>
   <div>
     <h1>Hey There! From Nuxt</h1>
+    <button @click="fetchTodoList">Fetch ToDos</button>
     <ul>
       <li v-for="todo in todoList" :key="`todo-id-${todo.id}`">
         <input type="checkbox" :checked="todo.completed" />
