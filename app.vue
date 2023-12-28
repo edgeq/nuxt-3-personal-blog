dab85d74925d0ecd4589c5282532c40ce82a2902
Edge Quintanilla <equintanilla@discoveryed.com>
2023-12-28 10:08:35 -0600

    nuxt fundamentals init commit

---
+++ app.vue
@@ -0,0 +1,34 @@
+<script>
+import { defineNuxtComponent } from '#app'
+
+export default defineNuxtComponent({
+  data: () => {({
+      todoList: []
+    })
+  },
+  methods: {
+    fetchTodoList() {
+      fetch('https://jsonplaceholder.typicode.com/todos')
+        .then(response => response.json())
+        .then(json => {
+          this.todoList = json
+        });
+    }
+  },
+  mounted() {
+    this.fetchTodoList();
+  }
+})
+</script>
+
+<template>
+  <div>
+    <h1>Hey There! From Nuxt</h1>
+    <ul>
+      <li v-for="todo in todoList" :key="`todo-id-${todo.id}`">
+        <input type="checkbox" :checked="todo.completed" />
+        {{ todo.title }}
+      </li>
+    </ul>
+  </div>
+</template>
