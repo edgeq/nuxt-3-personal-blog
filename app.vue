b159866fe67bd2e1a18996dc6b16e0ee41fcf2e6
Edge Quintanilla <equintanilla@discoveryed.com>
2023-12-28 11:30:53 -0600

    composotion api for a photo gallery

--- app.vue
+++ app.vue
@@ -1,34 +1,46 @@
-<script>
-import { defineNuxtComponent } from '#app'
+<script setup>
+import { computed, ref } from 'vue'
 
-export default defineNuxtComponent({
-  data: () => {
-    return {
-      todoList: []
-    }
-  },
-  methods: {
-    fetchTodoList() {
-      fetch('https://jsonplaceholder.typicode.com/todos')
-        .then(response => response.json())
-        .then(json => {
-          this.todoList = json
-        });
-    }
-  },
-  mounted() {
-    this.fetchTodoList();
-  }
+// https://vuejs.org/guide/essentials/reactivity-fundamentals.html
+// ref vs reactive
+// ref is recommended for most data types because reactive is limited to object types
+// and reactivity is lost when destructuring or replacing
+// see link for more info:
+// https://vuejs.org/guide/essentials/reactivity-fundamentals.html#limitations-of-reactive
+let photoGallery = ref([]);
+
+const numberOfPhotos = computed(() => {
+  return photoGallery.value.length;
 })
+
+const evenAlbums = computed(() => {
+  return photoGallery.value.filter(item => item.albumId % 2)
+})
+
+const oddAlbums = computed(() => {
+  return photoGallery.value.filter(item => !(item.albumId % 2))
+})
+
+function fetchPhotoGallery() {
+  fetch('https://jsonplaceholder.typicode.com/photos')
+    .then(response => response.json())
+    .then(json => {
+      photoGallery.value = json
+      window.console.log(photoGallery.value)
+    })
+}
 </script>
 
 <template>
   <div>
-    <h1>Hey There! From Nuxt</h1>
+    <h1>Photo Gallery</h1>
+    <button @click="fetchPhotoGallery">Fetch Data</button>
+    <p>
+      {{ numberOfPhotos }} photos ({{ oddAlbums?.length }} odd albums | {{ evenAlbums?.length }} even albums)
+    </p>
     <ul>
-      <li v-for="todo in todoList" :key="`todo-id-${todo.id}`">
-        <input type="checkbox" :checked="todo.completed" />
-        {{ todo.title }}
+      <li v-for="photo in photoGallery" :key="photo.id">
+        <img :src="photo.thumbnailUrl" :alt="photo.title">
       </li>
     </ul>
   </div>
