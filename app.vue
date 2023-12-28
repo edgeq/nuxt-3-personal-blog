4ed93c89aaa3a2b824c29b03d8aeb8969fcbdb59
Edge Quintanilla <equintanilla@discoveryed.com>
2023-12-28 11:48:15 -0600

    adding computed values

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <script setup>
+import { computed, ref } from 'vue'
+
 let todoList = ref([]);
+
+const completedItems = computed(() => {
+  return todoList.value.filter(item => item.completed)
+})
+
+const remainingItems = computed(() => {
+  return todoList.value.filter(item => !item.completed)
+})
 
 function fetchTodoList() {
   fetch('https://jsonplaceholder.typicode.com/todos')
@@ -11,8 +21,9 @@ function fetchTodoList() {
 </script>
 
 <template>
-  <h1>Hey There! From Nuxt</h1>
+  <h1>TODO LIST</h1>
   <button @click="fetchTodoList">Fetch ToDos</button>
+  <p>Complete: {{ completedItems.length }} | Remaining: {{ remainingItems.length }}</p>
   <ul>
     <li v-for="todo in todoList" :key="`todo-id-${todo.id}`">
       <input type="checkbox" :checked="todo.completed" />
