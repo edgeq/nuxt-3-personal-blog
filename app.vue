6b29eddf6fab7d8e3ea81103302f96854a069367
Edge Quintanilla <equintanilla@discoveryed.com>
2023-12-28 12:22:00 -0600

    styling with modules

--- app.vue
+++ app.vue
@@ -24,10 +24,27 @@ function fetchTodoList() {
   <h1>TODO LIST</h1>
   <button @click="fetchTodoList">Fetch ToDos</button>
   <p>Complete: {{ completedItems.length }} | Remaining: {{ remainingItems.length }}</p>
-  <ul>
+  <ul :class="todo.listItems">
     <li v-for="todo in todoList" :key="`todo-id-${todo.id}`">
       <input type="checkbox" :checked="todo.completed" />
       {{ todo.title }}
     </li>
   </ul>
 </template>
+
+<style module="todo">
+/* 
+  modules are a way to scope css - note the use of camelCase classNames
+    https://vuejs.org/api/sfc-css-features.html#css-modules
+    https://github.com/css-modules/css-modules?tab=readme-ov-file#naming
+
+  you can also use scoped - this adds data attributes to class-names
+    https://vuejs.org/api/sfc-css-features.html#scoped-css
+
+  or you can use naming conventions like BEM which is more of a convention/disgression
+*/
+.listItems {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+}
+</style>
