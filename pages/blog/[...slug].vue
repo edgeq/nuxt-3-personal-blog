59f9d075fcf2a2e93a3711a7e2f9187a38fea06b
Edge Quintanilla <equintanilla@discoveryed.com>
2024-01-03 17:02:25 -0600

    minor styling fixes for blog posts

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,7 +1,12 @@
 <template>
     <div class="container">
         <div class="content">
-            <ContentDoc />
+            <ContentDoc v-slot="{ doc }">
+                <article>
+                    <ContentRenderer :value="doc" />
+                </article>
+            </ContentDoc>
+
         </div>
     </div>
 </template>
