0d82aadf44968aa88c8821f4007b707ef62e8337
Edge Quintanilla <equintanilla@discoveryed.com>
2024-01-04 17:19:00 -0600

    copying a blog to see how ContentDoc interprets markup

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,14 +1,13 @@
 <script setup>
-const { data: posts } = await useAsyncData('blog', () => queryContent('blog').find()) 
-console.log(posts.value)
-
+const { data: posts } = await useAsyncData('blog', () => queryContent('blog').find())
+console.log(posts.value.length)
 </script>
 <template>
     <div class="hero-body container">
         <div class="content">
             <h1>Blog Posts</h1>
             <ul>
-                <li v-for="post in posts">
+                <li v-for="post in posts" :key="post.title">
                     <NuxtLink :to="post._path">{{ post.title }}</NuxtLink>
                 </li>
             </ul>
