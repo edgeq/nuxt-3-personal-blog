769f8c15cf257f2ac471f7d2db55a17479182e91
Edge Quintanilla <equintanilla@discoveryed.com>
2024-01-07 15:47:10 -0600

    more content and components for blog navigation

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,12 +1,41 @@
-<template>
-    <div class="hero-body container">
-        <div class="content">
-            <ContentDoc v-slot="{ doc }">
-                <article>
-                    <ContentRenderer :value="doc" />
-                </article>
-            </ContentDoc>
+<script setup>
 
-        </div>
-    </div>
+</script>
+<template>
+    <NuxtLink to="/blog" :class="blog.backButton">
+        <IconArrowLeft />
+    </NuxtLink> 
+    <section :class="['hero-body', 'container', blog.blog_container]">
+        <ContentDoc v-slot="{ doc }">
+            <article :class="['content', blog.blog_content]">
+                <ContentRenderer :value="doc" />
+            </article>
+            <aside :class="blog.toc" v-if="doc.body?.toc?.links.length">
+                <TableOfContents :toc="doc.body?.toc?.links" />
+            </aside>
+        </ContentDoc>
+    </section>
 </template>
+
+<style lang="scss" module="blog">
+.blog {
+    &_container {
+        display: flex;
+        gap: 2rem;
+    }
+    &_content {
+        width: 66%;
+        display: flex;
+        align-items: flex-start;
+    }
+}
+.backButton {
+    position: absolute;;
+    top: 6.75rem;
+    left: 2rem;
+    z-index: 2;
+}
+.toc {
+    width: 33%;
+}
+</style>
