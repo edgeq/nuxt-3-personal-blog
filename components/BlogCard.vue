8fc46f00b72240ddbb6531fb2594da77787123d2
Edge Quintanilla <equintanilla@discoveryed.com>
2024-01-21 14:11:45 -0600

    using ContentList for blog list and trying our useDateFormat

--- components/BlogCard.vue
+++ components/BlogCard.vue
@@ -1,16 +1,22 @@
 <script setup>
+import { useDateFormat } from '@vueuse/core'
+
 const props = defineProps({
     post: {
         type: Object,
         required: true,
     }
 })
-console.log()
+const date = computed(() => {
+    console.log(props.post.date);
+    return useDateFormat(props.post.date, 'MMMM DD, YYYY')
+})
 </script>
 
 <template>
     <div class="card">
         <div class="card-content">
+            <span class="tag is-dark">{{ date }}</span>
             <p class="title">
                 {{ $props.post.headline }}
             </p>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,19 +1,23 @@
 <script setup>
-const { data: posts } = await useAsyncData('blog', () => queryContent('blog').find())
+// Composables are an option...
+// const { data: posts } = await useAsyncData('blog', () => queryContent('blog').find())
 </script>
 <template>
-    <div class="hero-body container">
-        <div class="content">
-            <h1>Blog Posts</h1>
-            <ul class="blog-list">
-                <li class="blog-list-item" v-for="post in posts" :key="post.headline">
-                    <NuxtLink class="blog-list-item-link" :to="post._path">
-                        <BlogCard :post="post"/>
-                    </NuxtLink>
-                </li>
-            </ul>
+    <!-- But ContentList is a neat component that queries your contente list too! -->
+    <ContentList path="/blog" v-slot="{ list }">
+        <div class="hero-body container">
+            <div class="content">
+                <h1>Blog Posts</h1>
+                <ul class="blog-list">
+                    <li class="blog-list-item" v-for="post in list" :key="post.headline">
+                        <NuxtLink class="blog-list-item-link" :to="post._path">
+                            <BlogCard :post="post"/>
+                        </NuxtLink>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </div>
+    </ContentList>
 </template>
 
 <style lang="scss">
